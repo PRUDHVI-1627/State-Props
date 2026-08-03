d627400a1144d64cd4af865d8be02ebc541a7e8b
GALLA PRUDHVI RAJ <pruney1627@gmail.com>
2026-08-03 10:32:59 +0530

    added question-6(useReducer)

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,10 @@ import "./index.css";
 //import App from "./question4/App";
 
 //QUESTION-5
-import App from "./question5/App";
+//import App from "./question5/App";
+
+//QUESTION-6(useReducer)
+import App from "./question6(useReducer)/App.jsx";
 
 
 createRoot(document.getElementById("root")).render(
---
+++ src/question6(useReducer)/App.jsx
@@ -0,0 +1,100 @@
+/*
+import React, { useReducer, useState } from "react";
+
+const App = () => {
+  function reducer(state, action) {
+    if (action.type === "inc") {
+      return state + 1;
+    } else if (action.type === "dec") {
+      return state - 1;
+    } else if (action.type === "reset") {
+      return 0;
+    } else {
+      return state;
+    }
+  }
+
+  const [list, setList] = useState([]);
+  const [text, setText] = useState("");
+
+  function addList() {
+    if (text.trim() === "") return;
+
+    setList([...list, text]);
+    setText("");
+  }
+
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div>
+      <h2>{state}</h2>
+
+      <button onClick={() => dispatch({ type: "dec" })}>Dec</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <button onClick={() => dispatch({ type: "inc" })}>Inc</button>
+
+      <hr />
+
+      <h1>To-Do</h1>
+
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button onClick={addList}>Add</button>
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
+*/
+
+import React, { useReducer } from 'react'
+
+const App = () => {
+    let data={
+        input:"",
+        list:[]
+    }
+    function reduser(state,action){
+        if(action.type=="inp"){
+            return{
+                ...state,
+                input:action.payload
+            }
+        }
+        else if(action.type=="add"){
+            return{
+                ...state,
+                list:[...state.list,state.input]
+            }
+        }
+    }
+
+    let [state,dispatch]=  useReducer(reduser,data)
+
+  return (
+    <div>
+        <input  onChange={(e)=>dispatch({type:"inp",payload:e.target.value})}/>
+       <button  onClick={()=>dispatch({type:"add"})}>add</button>
+       {
+        state.list.map((a)=>{
+            return(<>
+            <li>{a}</li>
+            </>)
+        })
+       }
+    </div>
+  )
+}
+
+export default App
